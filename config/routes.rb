82b6aae8f819967c3c984fa96424c2957058e2f9
Yamamoto <shinji.yamamoto0828@gmail.com>
2019-01-01 23:30:05 +0900

    login function

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,10 @@ Rails.application.routes.draw do
   get "/about" => "static_pages#about"
   get "/contact" => "static_pages#contact"
   get "/signup" => "users#new"
+  get "/login" => "sessions#new"
+  post "/login" => "sessions#create"
+  post "/logout" => "sessions#destroy"
+
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
